425e8420acbcca47c24a472148724a7291b066b7
Hanna <hanna@MacBook-Air-Hanna.local>
2021-03-26 00:51:35 +0300

    add validation

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="login-page">
     <div class="form">
+      <p v-if="errors.length">
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+      </p>
       <form @submit.prevent="userLogin">
         <div>
           <label>Username</label>
@@ -25,22 +30,36 @@ export default {
       login: {
         username: '',
         password: ''
-      }
+      },
+      errors: []
     }
   },
   methods: {
     async userLogin () {
+      this.errors = []
       if (this.login.username && this.login.password) {
         try {
           const response = await this.$axios.$get('/login_user.json')
           console.log(response)
-          if (this.login.username === response.username && this.login.password === response.password) {
+          if (
+            this.login.username === response.username &&
+            this.login.password === response.password
+          ) {
             this.$store.commit('addToken', response.token)
             this.$router.push('/')
+          } else {
+            this.errors.push('Wrong username or password')
           }
         } catch (err) {
           console.log(err)
         }
+      }
+
+      if (!this.login.username) {
+        this.errors.push('Username is required')
+      }
+      if (!this.login.password) {
+        this.errors.push('Password is required')
       }
     }
   }
