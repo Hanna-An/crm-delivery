90159c35e62fbcfff49ec97b1324333a5daed50a
Maksim <blacklivi@yandex.by>
2021-03-26 19:06:35 +0300

    pop-up

--- pages/index.vue
+++ pages/index.vue
@@ -64,9 +64,20 @@
               {{ obj.ip_address }}
             </td>
             <td data-label="Edit">
-              <button class="icon">
-                &#128395;
+              <button class="box">
+                <a class="button" href="#popup1">&#128395;</a>
               </button>
+
+              <div id="popup1" class="overlay">
+                <div class="popup">
+                  <h2>Привет)</h2>
+                  <a class="close" href="#">&times;</a>
+                  <div class="content">
+                    Заботливый Максим, ожидая пока жена соберется решил по быстрону сделать pop-up.
+                    Дальше сам застилизуешь)
+                  </div>
+                </div>
+              </div>
             </td>
             <td data-label="Delete">
               <button
@@ -265,6 +276,83 @@ table th {
 
   table td:last-child {
     border-bottom: 0;
+  }
+}
+
+.box {
+  width: 40%;
+  margin: 0 auto;
+  background: rgba(255,255,255,0.2);
+  padding: 35px;
+  border: 2px solid #fff;
+  border-radius: 20px/50px;
+  background-clip: padding-box;
+  text-align: center;
+}
+
+.button {
+  font-size: 17px;
+  color: #fff;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.3s ease-out;
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+  transition: opacity 500ms;
+  visibility: hidden;
+  opacity: 0;
+}
+.overlay:target {
+  visibility: visible;
+  opacity: 1;
+}
+
+.popup {
+  margin: 70px auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  width: 30%;
+  position: relative;
+  transition: all 5s ease-in-out;
+}
+
+.popup h2 {
+  margin-top: 0;
+  color: #333;
+  font-family: Tahoma, Arial, sans-serif;
+}
+.popup .close {
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  transition: all 200ms;
+  font-size: 30px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #333;
+}
+.popup .close:hover {
+  color: #06D85F;
+}
+.popup .content {
+  max-height: 30%;
+  overflow: auto;
+}
+
+@media screen and (max-width: 700px){
+  .box{
+    width: 70%;
+  }
+  .popup{
+    width: 70%;
   }
 }
 </style>
