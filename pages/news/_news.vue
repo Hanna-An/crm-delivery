27f95c648037a723a47a151f895de00ad69ca72b
Diana <id789366@gmail.com>
2021-04-19 18:02:39 +0200

    news detail page

---
+++ pages/news/_news.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container py-7">
+    <div v-html="newsItem.content" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newsItem: ''
+    }
+  },
+  async created () {
+    const newsUrl = this.$route.params.news
+
+    const data = await this.$axios.$get('/news.json')
+    const news = data.data.news.items
+    if (news && news.length > 0) {
+      this.newsItem = news.find(elem => elem.url === newsUrl)
+    }
+  }
+}
+</script>
